074d406feea52455daf3aa34e52c3b1e688ef69c
Kertoo <piochl@st.amu.edu.pl>
2022-07-21 11:38:09 +0200

    Scrapping update + fisrt season

--- code/ScrapingWNZ.R
+++ code/ScrapingWNZ.R
@@ -13,7 +13,7 @@ seleniumServer <- rsDriver(browser = "chrome",
                            #verbose = FALSE, 
                            chromever = "103.0.5060.134", # the lastest
                            port = free_port())
-# Cilint object
+# Client object
 remDr <- seleniumServer$client
 
 remDr$open()
@@ -22,6 +22,10 @@ remDr$navigate(url)
 # click on cookies info:
 cookies <- remDr$findElement(using = "xpath", '//*[@id="onetrust-reject-all-handler"]')
 cookies$clickElement()
+# click on more info:
+moreInfo <- remDr$findElement(using = "xpath", '//*[@id="live-table"]/div[1]/div/div/a')
+moreInfo$clickElement()
+moreInfo$clickElement() # may possibly cause an error if 
 # get urls to match statistics
 obj <- remDr$findElement(using = "xpath", '//*[@id="live-table"]/div[1]/div/div')
 subSiteUrl <- 
@@ -32,14 +36,71 @@ subSiteUrl <- paste0("https://www.wynikinazywo.pl/mecz/", subSiteUrl, "/#/szczeg
 
 # navigate to match statistics:
 
-remDr$navigate(subSiteUrl[1])
-
-obj1 <- remDr$findElements(using = "class name", "stat__category")
-
-data.frame(
-  home = sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__homeValue")$getElementText()}) %>% unlist(),
-  name = sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__categoryName")$getElementText()}) %>% unlist(),
-  away = sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__awayValue")$getElementText()}) %>% unlist()
+K <- NULL
+matchNames <- NULL
+statNames <- c(
+  "Posiadanie piłki", # All possible match statistics provided on site
+  "Sytuacje bramkowe",
+  "Strzały na bramkę",
+  "Strzały niecelne",
+  "Rzuty rożne",
+  "Spalone",
+  "Interwencje bramkarzy",
+  "Faule",
+  "Żółte kartki",
+  "Czerwone kartki",
+  "Rzuty wolne",
+  "Auty bramkowe"
 )
+for (m in subSiteUrl[1:115]) {# Not all maches in 2012/2013 have match statistics this is for ones with them
+  print(m)                    # sometimes selenium breajs because of cookies it is possible to just start the loop again
+  remDr$navigate(m)           # beggining at which(subSiteUrl == m) no issues should be present
+  #if (which(subSiteUrl == m) == 1) {remDr$findElement(using = "id", "onetrust-reject-all-handler")$clickElement()} #this clicks cookies
+  
+  obj1 <- remDr$findElements(using = "class name", "stat__category")
+  if (length(obj1) == 0) {
+    obj1 <- remDr$findElements(using = "class name", "stat__category") # sometimes you need to double click
+  }
+  outcome <- strsplit(x = (obj1[[1]]$getTitle())[[1]], split = "|", fixed = TRUE)[[1]]
+  outcome1 <- suppressWarnings((outcome[1] %>% strsplit(split = ""))[[1]] %>% as.numeric())
+  matchNames <- c(matchNames, outcome[2] %>% str_trim())
+  outcome1 <- outcome1[!is.na(outcome1)]
+  valuesHome <- sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__homeValue")$getElementText()}) %>% unlist()
+  names(valuesHome) <- sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__categoryName")$getElementText()}) %>% unlist()
+  valuesHome <- ifelse(statNames %in% names(valuesHome), valuesHome, 0)
+  valuesAway <- sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__awayValue")$getElementText()}) %>% unlist()
+  names(valuesAway) <- sapply(obj1, FUN = function(x) {x$findChildElement(using = "class name", "stat__categoryName")$getElementText()}) %>% unlist()
+  valuesAway <- ifelse(statNames %in% names(valuesAway), valuesAway, 0)
+  participants <- stringr::str_trim(strsplit(outcome[2], split = "-")[[1]], side = "both")
+  if (length(participants) == 3) {                                                 # fix for podbeskidzie bielsko biała
+    if (which(participants == "B") == 3) {                                         # (podbeskidzie B-B)
+      participants <- c(participants[1], paste0(participants[2], participants[3]))
+    } else if (which(participants == "B") == 2) {
+      participants <- c(paste0(participants[1], participants[2]), participants[3])
+    } else {
+      stop("Check participants")
+    }
+  }
+  
+  if (is.null(K)) {
+    K <- c(valuesHome, valuesAway,
+           outcome1[1], outcome1[2], participants,
+           ifelse(outcome1[1] > outcome1[2], "H", ifelse(outcome1[2] > outcome1[1], "A", "D")),
+           remDr$findElement(using = "class name", "duelParticipant__startTime")$getElementText()[[1]]) %>%
+      data.frame()
+  } else {
+    K <- data.frame(
+      K,
+      c(valuesHome, valuesAway,
+        outcome1[1], outcome1[2], participants,
+        ifelse(outcome1[1] > outcome1[2], "H", ifelse(outcome1[2] > outcome1[1], "A", "D")),
+        remDr$findElement(using = "class name", "duelParticipant__startTime")$getElementText()[[1]])
+    )
+  }
+}
+
+K <- t(K)
+colnames(K) <- c(paste0(statNames, "Gospodarz"), paste0(statNames, "Gość"), "Gole Gospodarz", "Gole Gość", "Gospodarz", "Gość", "Wynik", "Data")
+rownames(K) <- matchNames
 
 # Tomorrow I'll do this for all elements in 2012 and hopefully other seasons won't be too difficult
